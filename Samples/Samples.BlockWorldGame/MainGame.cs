fc7aa905d3bc3ae433facb4d2119df511df77409
Will Craftia <willcraftia@gmail.com>
2013-06-24 19:53:07 +0900

    サンプル実装を開始。

---
+++ Samples/Samples.BlockWorldGame/MainGame.cs
@@ -0,0 +1,84 @@
+﻿#region Using
+
+using System;
+using Libra.Games;
+using Libra.Graphics;
+using Libra.Xnb;
+
+#endregion
+
+namespace Samples.BlockWorldGame
+{
+    public sealed class MainGame : Game
+    {
+        /// <summary>
+        /// ウィンドウの幅。
+        /// </summary>
+        const int WindowWidth = 800;
+
+        /// <summary>
+        /// ウィンドウの高さ。
+        /// </summary>
+        const int WindowHeight = 480;
+
+        /// <summary>
+        /// Libra のグラフィックス マネージャ。
+        /// </summary>
+        GraphicsManager graphicsManager;
+
+        /// <summary>
+        /// Libra の XNA コンテント マネージャ。
+        /// </summary>
+        XnbManager content;
+
+        /// <summary>
+        /// 描画に使用するコンテキスト。
+        /// </summary>
+        DeviceContext context;
+
+        /// <summary>
+        /// スプライト バッチ。
+        /// </summary>
+        SpriteBatch spriteBatch;
+
+        /// <summary>
+        /// フォント。
+        /// </summary>
+        SpriteFont spriteFont;
+
+        public MainGame()
+        {
+            graphicsManager = new GraphicsManager(this);
+
+            content = new XnbManager(Services, "Content");
+
+            graphicsManager.PreferredBackBufferWidth = WindowWidth;
+            graphicsManager.PreferredBackBufferHeight = WindowHeight;
+        }
+
+        protected override void LoadContent()
+        {
+            context = Device.ImmediateContext;
+
+            spriteBatch = new SpriteBatch(context);
+            spriteFont = content.Load<SpriteFont>("hudFont");
+
+        }
+    }
+
+    #region Program
+
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MainGame())
+            {
+                game.Run();
+            }
+        }
+    }
+
+    #endregion
+}
